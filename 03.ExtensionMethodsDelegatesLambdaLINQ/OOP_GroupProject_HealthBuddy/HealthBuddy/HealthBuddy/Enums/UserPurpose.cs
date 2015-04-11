f1f79038a7c6ba62df9a58cfcd41ea4200b57603
Maskln <lyubomir.nanev@gmail.com>
2015-04-11 15:03:41 +0300

    Team Work OOP

---
+++ 03.ExtensionMethodsDelegatesLambdaLINQ/OOP_GroupProject_HealthBuddy/HealthBuddy/HealthBuddy/Enums/UserPurpose.cs
@@ -0,0 +1,7 @@
+﻿namespace HealthBuddy
+{
+    public enum UserPurpose
+    {
+        Gain_Weight, Loose_Weight, Keep_Weight
+    }
+}
---
+++ 03.ExtensionMethodsDelegatesLambdaLINQ/OOP_GroupProject_HealthBuddy/HealthBuddy/HealthBuddy/Interfaces/IDietCalculator.cs
@@ -0,0 +1,11 @@
+﻿namespace HealthBuddy.Interfaces
+{
+    public interface IDietCalculator
+    {
+         int AgeOfPerson { get; set; }
+
+         double HeightOfPerson { get; set; }
+
+         double WeightOfPerson { get; set; }
+    }
+}
